df2cad76c47e4ee0899229b05c41e18dfe41601b
Anthony Aruanna <24880946+C4vemann@users.noreply.github.com>
2023-02-14 22:01:03 -0500

    Add files via upload

--- script4.js
+++ script4.js
@@ -1,15 +1,17 @@
 function DoublyLinkedList(){
-	let root = null;
-	let pointer = null;
+	let root;
+	let pointer;
+	let length;
+	constructor();
 
 	function Node(n){
 		let next;
-		let name;
+		let id;
 		constructor(n);
 
 		function constructor(n){
 			next = null;
-			name = n;
+			id = n;
 		}
 
 		function getNext(){
@@ -20,35 +22,60 @@ function DoublyLinkedList(){
 			next = node;
 		}
 
-		function getName(){
-			return name;
+		function getId(){
+			return id;
 		}
 
-		function setName(n){
-			name = n;
+		function setId(n){
+			id = n;
 		}
 
 		return{
 			getNext,
-			getName,
+			getId,
 			setNext,
-			setName
+			setId
 		}
 	}
 
-	function push(name){
-		if(root == null){
-			root = Node(name);
+	function constructor(){
+		root = null;
+		pointer = null;
+		length = 0;
+	}
+
+	//push a node at the end of the list with the value of id
+	//returns false if there is no id present
+	//returns true if node was successfully inserted
+	function push(id){
+		if(id === null | id === undefined) return false;
+		if(root === null){
+			root = Node(id);
 			pointer = root;
 		} else {
-			pointer.setNext(Node(name));
+			pointer.setNext(Node(id));
 			pointer = pointer.getNext();
 		}
+		length = length + 1;
+		return true;
 	}
 
+	//pops a node based off its id
+	//returns false if there is no id
+	//returns false if there is nothing in the list 
+	//returns true if the node was successfully removed
 	function pop(id){
-		if(root == null) return;
+		if(id === null | id === undefined) return false;
+		if(root === null) return false;
+		if(length === 0) return false;
+
 		pointer = root;
+
+		if(pointer.getId() === id) root = pointer.getNext();
+		pointer.getNext();
+
+		if(pointer.getId() === id) pointer.setNext(pointer.getNext());
+
 		while(true){
 			if(!pointer.getName() == id) continue;
 			else{
@@ -59,16 +86,44 @@ function DoublyLinkedList(){
 		}
 	}
 
-	function getSize(){
-		let counter = 0;
-		if(root == null) return counter;
+	//finds a node based on its id
+	//returns false if there is no id
+	//returns false if there is nothing in the list
+	//returns false if it did not find the id
+	//returns the index of the found id when successful
+	function find(id){
+		if(id === null | id === undefined) return false;
+		if(root === null) return false;
+		if(length === 0) return false;
+
 		pointer = root;
-		while(true){
-			counter++;
-			if(pointer.getNext() == null) break;
+
+		let i;
+		for(i = 0; i < length; i++){
+			if(pointer.getId() === id) break;
 			pointer = pointer.getNext();
 		}
-		return counter;
+
+		if(i === length) return false;
+		return i;
+	}
+
+
+
+/*	function set(id,position){
+		if(root === null) return;
+		if(id === null | id === undefined) return;
+
+		let temp = Node(id);
+		pointer = root;
+
+		for(let i = 0; i < position; i++){
+			pointer = pointer.getNext();
+		}
+	}*/
+
+	function getLength(){
+		return length;
 	}
 
 	function getRoot(){
@@ -81,9 +136,10 @@ function DoublyLinkedList(){
 
 	return{
 		push,
+		pop,
+		find,
 		getRoot,
 		getPointer,
-		getSize,
-		pop
+		getLength,
 	}
 }
