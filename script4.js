59566b4ca9f24f6ff517d96dd3eda5914240a258
Anthony Aruanna <24880946+C4vemann@users.noreply.github.com>
2023-02-17 19:31:00 -0500

    Add files via upload

--- script4.js
+++ script4.js
@@ -88,62 +88,102 @@ function LinkedList(){
 				length--;
 				return true;
 			}
-		}while((pointer = pointer.getNext()) != null);
+		}while((pointer = pointer.getNext()) !== null);
 
 
 	}
-	function traverse(){
-		let count = 0;
+
+	//returns false if pointer is null
+	//returns false if root is null
+	//returns false if length is null
+	//returns false if value entered is larger that size of linked list
+	//returns false if every value of string parameter has been checked and the final one checked doesn't match
+	//returns id of pointer if true
+	//returns pointer.getId() only for testing purposes
+	//purpose of this function is to set the pointer to an index in the linked list
+	//returns pointer
+	function traverse(value){
+		if(root === null | length === 0 | pointer === null) return false;	
 
 		pointer = root;
 
-		//lands on the last element
-		while(pointer.getNext() != null){
-			console.log(pointer.getId());
-			pointer = pointer.getNext();
-			count++;
+		//traverses to the last element
+		if(value === undefined | value === null){
+			while(pointer.getNext() !== null){
+				pointer = pointer.getNext();
+			}
+			return pointer;
 		}
 
-		return pointer.getId();
+		//traverse to the element specified by idss
+		if(typeof value === 'string'){
+			while(pointer.getNext() !== null){
+				if(pointer.getId() === value) break;
+				pointer = pointer.getNext();
+			}
+			if(pointer.getId() !== value) return false;
+			return pointer;
+		}
+
+		//traverse to the element specified by value
+		if(typeof value === 'number'){
+			if(value >= length) return false;
+			let count = 0;
+			while(pointer.getNext() !== null){
+				if(count === value) break;
+				pointer = pointer.getNext();
+				count++;
+			}
+			return pointer;
+		}
+
+
 	}
 
-	//finds a node based on its id
-	//returns false if there is no id
-	//returns false if there is nothing in the list
-	//returns false if it did not find the id
-	//returns the index of the found id when successful
-	function find(id){
-		if(id === null | id === undefined) return false;
-		if(root === null) return false;
-		if(length === 0) return false;
 
-		pointer = root;
+	//returns false if root is null
+	//returns false if pointer is null
+	//returns false if id is undefined
+	//returns false if id is null
+	//returns true if setting new node is successful
+	function set(id,position){
+		if(root === null | pointer === null | length === 0 | id === undefined | id === null) return false;
 
-		let count = 0;
-		
-		while(true){
-			if(pointer.getId() === id) break;
-			pointer = pointer.getNext();
+		if(position === 0){
+			let temp = root;
+			root = Node(id);
+			root.setNext(temp);
+			return true;
 		}
 
-		if(count === length) return false;
+		if(position === undefined | position === null | position > length - 1){
+			push(id);
+			return true;
+		}
 
-		return count;
+		pointer = traverse(position-1);
+
+		let temp = pointer.getNext();
+
+		pointer.setNext(Node(id));
+		pointer = pointer.getNext();
+		pointer.setNext(temp);
+
+		return true;
+
 	}
 
-
-
-/*	function set(id,position){
-		if(root === null) return;
-		if(id === null | id === undefined) return;
-
-		let temp = Node(id);
+	function print(){
+		if(root === null | pointer === null | length === 0) return "List is empty";
+		let string = "[";
 		pointer = root;
-
-		for(let i = 0; i < position; i++){
+		while(pointer.getNext() !== null){
+			string += "{" + pointer.getId() + "},";
 			pointer = pointer.getNext();
 		}
-	}*/
+		string += "{" + pointer.getId() + "}]";
+		return string;
+	}
 
 	function getLength(){
 		return length;
@@ -160,10 +200,21 @@ function LinkedList(){
 	return{
 		push,
 		pop,
-		find,
+		traverse,
+		set,	
+		print,	
 		getRoot,
 		getPointer,
 		getLength,
-		traverse
 	}
 }
+
+let x = LinkedList();
+x.push("Anthony");
+x.push("Joey");
+x.push("Maria");
+x.push("Rocco");
+x.push("Angela");
+x.push("Bruce");
+x.push("Mom");
+x.push("Dad");
